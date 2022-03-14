e0f3f4ff9c0caa761177c8ad67f6f8527b8ccf0a
bendance <bendance@iu.edu>
2022-03-13 23:25:07 -0400

    testing gitignore

---
+++ VagueUniversity-BE/VagueUniversity-BE/MailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace VagueUniversity_BE
+{
+    public class MailRequest
+    {
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? message { get; set; }
+    }
+}
